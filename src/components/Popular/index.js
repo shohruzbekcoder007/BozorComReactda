793b37e19e0796fa7c356b3cf378e1d743b5b1e7
shohruzbek <shohruzbekochilov007@gmail.com>
2021-12-09 17:34:13 +0500

    slider

--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -1,14 +1,104 @@
 import React from 'react'
 import {
     PoularWrapper,
-    PoularText
+    PoularText,
+    PopularItemWrapper,
+    PopularItemImage,
+    ItemImage,
+    PopularItemText,
+    ItemTextParagriph,
+    ItemTextInformation,
+    AllServices
 } from './styles'
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+import itemimage1 from './../../assets/img/itemimage1.png'
+import itemimage2 from './../../assets/img/itemimage2.png'
+import itemimage3 from './../../assets/img/itemimage3.png'
+
+
 
 const Popular = () => {
     return (
         <PoularWrapper>
             <PoularText>Popular services in your area</PoularText>
+            <PopularSlider/>
+            <AllServices>See All Services</AllServices>
         </PoularWrapper>
+    )
+}
+
+const PopularSlider = () => {
+
+    const settings = {
+        // dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true
+    }
+
+    return (
+        <div className="popular-slider">
+            <Slider {...settings}>
+                <PopularItem 
+                    image={itemimage1}
+                    paragriph="Bathroom Remodeling"
+                    information="we will make the old bathroom even better with 
+                    this service, and we are the greatest among them"
+                />
+                <PopularItem 
+                    image={itemimage2}
+                    paragriph="Bathroom Remodeling"
+                    information="we will make the old bathroom even better with 
+                    this service, and we are the greatest among them"
+                />
+                <PopularItem 
+                    image={itemimage3}
+                    paragriph="Bathroom Remodeling"
+                    information="we will make the old bathroom even better with 
+                    this service, and we are the greatest among them"
+                />
+                <PopularItem 
+                    image={itemimage1}
+                    paragriph="Bathroom Remodeling"
+                    information="we will make the old bathroom even better with 
+                    this service, and we are the greatest among them"
+                />
+                <PopularItem 
+                    image={itemimage2}
+                    paragriph="Bathroom Remodeling"
+                    information="we will make the old bathroom even better with 
+                    this service, and we are the greatest among them"
+                />
+                <PopularItem 
+                    image={itemimage3}
+                    paragriph="Bathroom Remodeling"
+                    information="we will make the old bathroom even better with 
+                    this service, and we are the greatest among them"
+                />
+            </Slider>
+        </div>
+    )
+}
+
+const PopularItem = ({image, paragriph,information}) => {
+    return(
+        <PopularItemWrapper>
+            <PopularItemImage>
+                <ItemImage src={image} alt="popular image item"/>
+            </PopularItemImage>
+            <PopularItemText>
+                <ItemTextParagriph>{paragriph}</ItemTextParagriph>
+                <ItemTextInformation>
+                    {information}
+                </ItemTextInformation>
+            </PopularItemText>
+        </PopularItemWrapper>
     )
 }
 
--- src/components/Popular/styles.js
+++ src/components/Popular/styles.js
@@ -15,4 +15,62 @@ export const PoularText = styled.p`
     text-transform: capitalize;
     color: #021633;
     text-align: center;
+    margin-bottom: 70px;
 `;
+
+export const PopularItemWrapper = styled.div`
+    width: 356px;
+    background: #FCFCFC;
+    border-radius: 10px;
+    padding: 15px;
+    ::hover: {
+        box-shadow: 0px 12px 35px rgba(5, 9, 19, 0.05);
+    }
+`;
+
+export const PopularItemImage = styled.div`
+    width: 326px;
+    height: 241px;
+`;
+
+export const ItemImage = styled.img`
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+`;
+
+export const PopularItemText = styled.div`
+    width: 100%;
+    height: 116px;
+    overflow: hidden;
+    margin-bottom: 9px
+`;
+
+export const ItemTextParagriph = styled.p`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 32px;
+    color: #021633;
+    margin-top: 20px;
+    margin-bottom: 12px;
+`;
+
+export const ItemTextInformation = styled.p`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 24px;
+    color: rgba(2, 22, 51, 0.5);
+`;
+
+export const AllServices = styled.p`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 32px;
+    text-align: center;
+    color: #001730;
+    margin-top: 87px;
+    cursor: pointer;
+`; 
